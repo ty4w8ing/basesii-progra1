e96dbdf3644dc53bc173455c1492ad6a5889437f
Gustavo <tavog14@gmail.com>
2017-04-14 19:40:41 -0600

    esqueleto de proyecto java

---
+++ java/src/main/java/tec/ac/cr/BasesiiApplication.java
@@ -0,0 +1,13 @@
+package tec.ac.cr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasesiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasesiiApplication.class, args);
+	}
+
+}
